f1bd76d9c5e3bab31ce1c777c14618fbd5301eaf
Andy Lyon <andy@ipolar.co.uk>
2012-03-17 09:43:32 +0000

    Updated the PhoneGap plugin to work with the new Cordova framework.

--- ScrimLoader.h
+++ ScrimLoader.h
@@ -1,24 +1,28 @@
 /*
   ScrimLoader.h
   bubblepuzzle
-
- Created by Nigel Kelly on 06/09/2011.
-Copyright 2011 Nigel Kelly. All rights reserved.
+  
+  Modified to run on the new Cordova (PhoneGap >1.5) framework by Andy Lyon on 17/03/2012
+  
+  Created by Nigel Kelly on 06/09/2011.
+  Copyright 2011 Nigel Kelly. All rights reserved.
  
-*/ 
-
+*/
 
 #import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-#import "PhoneGapCommand.h"
+#ifdef CORDOVA_FRAMEWORK
+#import <Cordova/CDVPlugin.h>
+#else
+#import "CDVPlugin.h"
+#endif
 
-
-@interface ScrimLoader: PhoneGapCommand {
-	
+@interface ScrimLoader: CDVPlugin {
+	NSString* callbackId;
 	UIView *scrimView;
 	UIActivityIndicatorView *myIndicator;
 }
 
+@property(nonatomic, copy) NSString* callbackId;
 @property(nonatomic, retain) UIView* scrimView;
 @property(nonatomic, retain) UIActivityIndicatorView* myIndicator;
 
@@ -26,5 +30,3 @@ Copyright 2011 Nigel Kelly. All rights reserved.
 - (void) dismissScrimLoader:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
 
 @end
-
-
--- ScrimLoader.m
+++ ScrimLoader.m
@@ -1,48 +1,46 @@
 /*
- ScrimLoader.m
- 
- Created by Nigel Kelly on 06/09/2011.
- Copyright 2011 Nigel Kelly. All rights reserved.
- 
- */ 
+  ScrimLoader.m
+  
+  Modified to run on the new Cordova (PhoneGap >1.5) framework by Andy Lyon on 17/03/2012
+  
+  Created by Nigel Kelly on 06/09/2011.
+  Copyright 2011 Nigel Kelly. All rights reserved.
+*/ 
 
 #import "ScrimLoader.h"
-
 
 @implementation ScrimLoader
 @synthesize scrimView;
 @synthesize myIndicator;
-
-
+@synthesize callbackId;
 
 - (void) showScrimLoader:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options 
 {
+	NSLog( @"[LOADER]		Show loader!");
+	
 	UIApplication* app = [UIApplication sharedApplication];
 	UIWindow* appWindow = [app keyWindow];
 	
 	self.scrimView = [[[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)] autorelease];
-	self.scrimView.backgroundColor = [UIColor clearColor];
+	self.scrimView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.7]; //[UIColor clearColor];
 	[appWindow addSubview:self.scrimView];
 	[appWindow makeKeyAndVisible];
 	
 	self.myIndicator = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge] autorelease];
-	self.myIndicator.center = CGPointMake(160, 440);
+	self.myIndicator.center = self.scrimView.center;
 	self.myIndicator.hidesWhenStopped = NO;
 	
 	[self.scrimView addSubview:self.myIndicator];
 	[self.myIndicator startAnimating];
-	
 }
 
 - (void) dismissScrimLoader:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options 
 {
-		
-	[self.myIndicator stopAnimating];
+	NSLog( @"[LOADER]		Hide loader!");
 	
+	[self.myIndicator stopAnimating];
 	[self.myIndicator removeFromSuperview];
 	[self.scrimView removeFromSuperview];
-
 }
-
 
 @end
